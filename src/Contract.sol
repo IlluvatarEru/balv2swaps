c53e5b1f95024e397733bea0ce62d156e73c21ab
Arthurim <arthurbdauphine@gmail.com>
2022-07-09 20:56:50 +0100

    updste

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,89 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+
+interface IERC20 {
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+enum SwapKind { GIVEN_IN, GIVEN_OUT }
+interface IAsset {
+    // solhint-disable-previous-line no-empty-blocks
+}
+struct SingleSwap {
+        bytes32 poolId;
+        SwapKind kind;
+        IAsset assetIn;
+        IAsset assetOut;
+        uint256 amount;
+        bytes userData;
+}
+
+struct SwapRequest {
+        SwapKind kind;
+        IERC20 tokenIn;
+        IERC20 tokenOut;
+        uint256 amount;
+        // Misc data
+        bytes32 poolId;
+        uint256 lastChangeBlock;
+        address from;
+        address to;
+        bytes userData;
+    }
+
+
+struct FundManagement {
+    address sender;
+    bool fromInternalBalance;
+    address payable recipient;
+    bool toInternalBalance;
+}    
+
+interface IVault {
+    function swap(
+        SingleSwap memory singleSwap,
+        FundManagement memory funds,
+        uint256 limit,
+        uint256 deadline
+    ) external payable returns (uint256);
+
+}
+
+address constant vaultAddress = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
+
+contract TestSwap {
+    address internal owner;
+
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function trade() public returns (uint256) {
+
+        IERC20 erc20 = IERC20(0x41e5560054824eA6B0732E656E3Ad64E20e94E45);
+        erc20.approve(vaultAddress,100000);
+
+        IVault vault = IVault(vaultAddress);
+
+        SingleSwap memory s = SingleSwap(
+          0x760afd4460089edbb77f71149f6fe1d7554c545000010000000000000000011d,
+          SwapKind.GIVEN_IN,
+          IAsset(0x41e5560054824eA6B0732E656E3Ad64E20e94E45),
+          IAsset(0x89Ab32156e46F46D02ade3FEcbe5Fc4243B9AAeD),
+          100000,
+          '0x'
+        );
+
+        FundManagement memory f = FundManagement(
+            address(this),
+            false,
+            payable(address(this)),
+            false
+        );
+
+        return vault.swap(s,f,100000,block.timestamp+10000);
+    }
+
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,50 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Contract.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    TestSwap c ;
+
+    function setUp() public {
+        c = new TestSwap();
+    }
 
     function testExample() public {
-        assertTrue(true);
+        uint256 amountIn=21665956598887613694231;
+        uint256 amountOut=1510168986908;
+        
+        address trader=0x9A315BdF513367C0377FB36545857d12e85813Ef;
+        vm.startPrank(trader);  
+
+        IERC20 erc20 = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        //erc20.approve(trader,100000);
+        //15,108,181
+        erc20.approve(vaultAddress,amountIn);
+
+        IVault vault = IVault(vaultAddress);
+
+        SingleSwap memory singleSwap = SingleSwap(
+          0xc45d42f801105e861e86658648e3678ad7aa70f900010000000000000000011e,
+          SwapKind.GIVEN_IN,
+          IAsset(0x6B175474E89094C44Da98b954EedeAC495271d0F),
+          IAsset(0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5),
+          amountIn,
+          '0x'
+        );
+
+        FundManagement memory funds = FundManagement(
+            trader,
+            false,
+            payable(trader),
+            false
+        );  
+        console.log("B TS",block.timestamp);
+        console.log("B NB",block.number);
+        uint256 amountOutReal= vault.swap(singleSwap,funds,1,block.timestamp+10000);
+        vm.stopPrank();
+        console.log("amountOutReal", amountOutReal);
+        assertTrue(amountOutReal>0);
     }
+
 }
